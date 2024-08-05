0e88f68cc2d7d6f6deffa653775b0e85cd2a635b
shotime036 <shotime036@gmail.com>
2024-08-05 20:32:42 +0900

    maqueen.hを更新
    maqueen.hファイルにmacro_definition.hのファイルの内容を追加

--- app_sample/maqueen.h
+++ app_sample/maqueen.h
@@ -16,6 +16,44 @@
 #define LIGHT_R 1
 
 // その他必要なdefineはここ
+#define ROBOT_DEFINES_H
+
+#define MAQUEEN 0x10
+#define LEFT_DIRECTION 0x00
+#define LEFT_SPEED 0x01
+#define RIGHT_DIRECTION 0x02
+#define RIGHT_SPEED 0x03
+#define SENSOR 0x1D
+
+#define L2 16
+#define L1 8
+#define M 4
+#define R1 2
+#define R2 1
+
+#define L2R2 17
+#define L2MR2 21
+#define L2MR1R2 23
+#define L2L1MR2 25
+#define L2L1MR1R2 31
+#define L1M 12
+#define MR1 6
+#define L1MR1 13
+#define MR2 5
+
+#define MR1R2 7
+#define L1MR2 13
+#define L1MR1R2 15
+#define L2M 20
+#define L2MR1 22
+#define L2L1M 28
+#define L2L1MR1 30
+
+#define FORWARD 0
+#define BACKWARD 1
+
+#define SENSOR_DELAY 100 // センサー読み取りの遅延時間（ミリ秒）
+
 
 ER maqueen_init();
 ER control_motor(B motor, B direction, B speed);
