bcd9b49d1c028c4f43c241e8294ab5b97ae5ed85
shotime036 <shotime036@gmail.com>
2024-08-12 20:58:06 +0900

    Update maqueen.c

--- app_sample/maqueen.c
+++ app_sample/maqueen.c
@@ -4,36 +4,30 @@
 
 // Maqueenの初期化
 ER maqueen_init(void) {
-    ER err;
-    // I2C通信の初期化
-    err = iicsetup(TRUE);
-    if (err != E_OK) {
-        return err;
-    }
-    return E_OK;
+	return iic_setup(TRUE);
 }
 
 // モーターを制御する関数
-ER control_motor(B motor, B direction, B speed) {
+ER control_motor(B motor, UB direction, UB speed) {
     ER err;
     if (motor == LEFT_MOTOR) {  // 左モーター
-        err = write_reg(EXTERNAL_I2C_CHANNEL, MAQUEEN_ADDRESS, LEFT_DIRECTION,  speed < 0 ? BACKWARD : FORWARD);
+        err = iic_write(EXTERNAL_I2C_CHANNEL, MAQUEEN_ADDRESS, LEFT_DIRECTION, direction);
         if (err != E_OK) return err;
-        err = write_reg(EXTERNAL_I2C_CHANNEL, MAQUEEN_ADDRESS, LEFT_SPEED, abs(speed));
+        err = iic_write(EXTERNAL_I2C_CHANNEL, MAQUEEN_ADDRESS, LEFT_SPEED, speed);
         if (err != E_OK) return err;
     } else if(motor == RIGHT_MOTOR){  // 右モーター
-        err = write_reg(EXTERNAL_I2C_CHANNEL, MAQUEEN_ADDRESS, RIGHT_DIRECTION, speed < 0 ? BACKWARD : FORWARD);
-        tm_printf("direction set\n");
+        err = iic_write(EXTERNAL_I2C_CHANNEL, MAQUEEN_ADDRESS, RIGHT_DIRECTION, direction);
         if (err != E_OK) return err;
-        err = write_reg(EXTERNAL_I2C_CHANNEL, MAQUEEN_ADDRESS, RIGHT_SPEED, abs(speed));
-        tm_printf("speed set\n");
+        err = iic_write(EXTERNAL_I2C_CHANNEL, MAQUEEN_ADDRESS, RIGHT_SPEED, speed);
         if (err != E_OK) return err;
     } else if(motor == BOTH_MOTOR){ //両方のモーター
-    	err = write_reg(EXTERNAL_I2C_CHANNEL, MAQUEEN_ADDRESS, RIGHT_DIRECTION, direction);
-    	err = write_reg(EXTERNAL_I2C_CHANNEL, MAQUEEN_ADDRESS, LEFT_DIRECTION,  speed < 0 ? BACKWARD : FORWARD);
+    	err = iic_write(EXTERNAL_I2C_CHANNEL, MAQUEEN_ADDRESS, LEFT_DIRECTION, direction);
     	if (err != E_OK) return err;
-    	err = write_reg(EXTERNAL_I2C_CHANNEL, MAQUEEN_ADDRESS, RIGHT_SPEED, abs(speed));
-    	err = write_reg(EXTERNAL_I2C_CHANNEL, MAQUEEN_ADDRESS, LEFT_SPEED, abs(speed));
+    	err = iic_write(EXTERNAL_I2C_CHANNEL, MAQUEEN_ADDRESS, RIGHT_DIRECTION, direction);
+    	if (err != E_OK) return err;
+    	err = iic_write(EXTERNAL_I2C_CHANNEL, MAQUEEN_ADDRESS, LEFT_SPEED, speed);
+    	if (err != E_OK) return err;
+    	err = iic_write(EXTERNAL_I2C_CHANNEL, MAQUEEN_ADDRESS, RIGHT_SPEED, speed);
     	if (err != E_OK) return err;
     }
     return E_OK;
@@ -43,9 +37,9 @@ ER control_motor(B motor, B direction, B speed) {
 // 全モーターを停止する関数
 ER stop_all_motor(void) {
     ER err;
-    err = write_reg(EXTERNAL_I2C_CHANNEL, MAQUEEN_ADDRESS, LEFT_SPEED, 0);  // 左モーター停止
+    err = iic_write(EXTERNAL_I2C_CHANNEL, MAQUEEN_ADDRESS, LEFT_SPEED, 0);  // 左モーター停止
     if (err != E_OK) return err;
-    err = write_reg(EXTERNAL_I2C_CHANNEL, MAQUEEN_ADDRESS, RIGHT_SPEED, 0);  // 右モーター停止
+    err = iic_write(EXTERNAL_I2C_CHANNEL, MAQUEEN_ADDRESS, RIGHT_SPEED, 0);  // 右モーター停止
     if (err != E_OK) return err;
     return E_OK;
 }
@@ -59,20 +53,18 @@ ER control_led(B led, B state) {
     if (led == MAQUEEN_LED_LEFT) {
         led_reg = MAQUEEN_LED_LEFT;  // 左LEDのレジスタアドレスを設定
     } else if (led == MAQUEEN_LED_RIGHT) {
-        led_reg = MAQUEEN_LED_LEFT;  // 右LEDのレジスタアドレスを設定
+        led_reg = MAQUEEN_LED_RIGHT;  // 右LEDのレジスタアドレスを設定
     } else {
         return E_PAR;  // 無効な引数の場合はエラーを返す
     }
 
-    // LEDをオンまたはオフにする
-    if (state == 1) {
-        err = write_reg(EXTERNAL_I2C_CHANNEL, MAQUEEN_ADDRESS, led_reg, 1);  // LEDをオン
-    } else if (state == 0) {
-        err = write_reg(EXTERNAL_I2C_CHANNEL, MAQUEEN_ADDRESS, led_reg, 0);  // LEDをオフ
-    } else {
+    // state が 0 か 1 以外の場合はエラーを返す
+    if (state > 1) {
         return E_PAR;  // 無効なstateの場合はエラーを返す
     }
 
+    // LEDをオンまたはオフにする
+    err = iic_write(EXTERNAL_I2C_CHANNEL, MAQUEEN_ADDRESS, led_reg, state);
     if (err != E_OK) {
         return err;  // エラーが発生した場合、エラーコードを返す
     }
@@ -80,30 +72,38 @@ ER control_led(B led, B state) {
     return E_OK;  // 正常終了
 }
 
-ER stop_all_led(void){
+
+ER turn_off_all_led(void){
 	ER err;
 	B led_reg;
 	B led_reg2;
 	led_reg = MAQUEEN_LED_LEFT;
 	led_reg2 = MAQUEEN_LED_RIGHT;
-	err = write_reg(EXTERNAL_I2C_CHANNEL, MAQUEEN_ADDRESS, led_reg, 0);
+	err = iic_write(EXTERNAL_I2C_CHANNEL, MAQUEEN_ADDRESS, led_reg, 0);
 	if (err != E_OK) return err;
-	err = write_reg(EXTERNAL_I2C_CHANNEL, MAQUEEN_ADDRESS, led_reg2, 0);
+	err = iic_write(EXTERNAL_I2C_CHANNEL, MAQUEEN_ADDRESS, led_reg2, 0);
 	if (err != E_OK) return err;
 	    return E_OK;
 }
 
 
-// ラインセンサーの状態を読み取る関数01
+/*// ラインセンサーの状態を読み取る関数01
 B read_line_state(B line) {
-    UB value = read_reg(1, MAQUEEN_ADDRESS,0x1D );
+    UB value = iic_read(1, MAQUEEN_ADDRESS,0x1D );
     UB value2 = value & (1<<line);
     return value2>>line;
+}*/
 
+B read_line_state(B line) {
+	return (iic_read(EXTERNAL_I2C_CHANNEL, MAQUEEN_ADDRESS, MAQUEEN_LINE_SENSOR_29) >> line) & 0x01;
+}
+//iic_read(1, MAQUEEN_ADDRESS, 0x1D) で値を読み取る
+//>> line で指定したライン番号のビットを最下位ビットに移動させる
+//& 0x01 でその最下位ビットのみを抽出する
+
+// ラインセンサーの値(0-256)を読み取る関数
+UB read_line_value(B ch, B line) {
+    return (UB)iic_read(EXTERNAL_I2C_CHANNEL, MAQUEEN_ADDRESS, MAQUEEN_LINE_SENSOR + 2*line);
 }
 
-// ラインセンサーの値を読み取る関数
-UB read_line_value(B line) {
-    return read_reg(1, MAQUEEN_ADDRESS, SENSOR + 2*line);
-}
 
