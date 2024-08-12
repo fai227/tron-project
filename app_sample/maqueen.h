76193c22121ce32c3ea8f3110857981f87577d71
shotime036 <shotime036@gmail.com>
2024-08-12 20:57:24 +0900

    Update maqueen.h

--- app_sample/maqueen.h
+++ app_sample/maqueen.h
@@ -9,18 +9,19 @@
 #define LEFT_SPEED      0x01
 #define RIGHT_DIRECTION 0x02
 #define RIGHT_SPEED     0x03
-#define SENSOR          0x1E//30
+#define MAQUEEN_LINE_SENSOR  0x1E//30
+#define MAQUEEN_LINE_SENSOR_29 0x1D
 
 // ライントレーサの定数
-#define L2  0
-#define L1  1
-#define M   2
-#define R1  3
-#define R2  4
+#define MAQUEEN_LINE_SENSOR_L2  0
+#define MAQUEEN_LINE_SENSOR_L1  1
+#define MAQUEEN_LINE_SENSOR_M   2
+#define MAQUEEN_LINE_SENSOR_R1  3
+#define MAQUEEN_LINE_SENSOR_R2  4
 
 // 進行方向の定義
-#define FORWARD     0
-#define BACKWARD    1
+#define MAQUEEN_MOVE_FORWARD     0
+#define MAQUEEN_MOVE_BACKWARD    1
 
 //LEDライトの定義
 #define MAQUEEN_LED_LEFT 0x0B
@@ -34,17 +35,16 @@
 //チャンネル設定
 #define EXTERNAL_I2C_CHANNEL 1
 
-// センサー読み取りの遅延時間（ミリ秒）
-#define SENSOR_DELAY 100
+
 
 // 関数プロトタイプ
 ER maqueen_init(void);
-ER control_motor(B motor, B direction, B speed);
+ER control_motor(B motor, UB direction, UB speed);
 ER stop_all_motor(void);
 ER control_led(B led, B state);
-ER stop_all_led(void);
+ER turn_off_all_led(void);
 B read_line_state(B line);
-UB read_line_value(B line);
+UB read_line_value(B ch, B line);
 
 
 #endif /* MAQUEEN_H_ */
