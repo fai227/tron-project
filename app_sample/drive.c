08306dcd420ebbb0cb4457a68b98b91d86531635
hiroyuki-tatsumi <166709271+hiroyuki-tatsumi@users.noreply.github.com>
2024-09-18 19:44:11 +0900

    Update drive.c
    タイマの編集

--- app_sample/drive.c
+++ app_sample/drive.c
@@ -8,94 +8,72 @@
 #include "LED.h"
 
 // 各区間の目標走行時間（ミリ秒）
-#define TARGET_INTERVAL 2000
-#define FORWARD_SPEED 35
-#define BACKWARD_SPEED 25
-#define STOP 0
+#define D_TARGET_INTERVAL 2000
+#define D_FORWARD_SPEED 35
+#define D_BACKWARD_SPEED 25
+#define D_STOP 0
 
-#define DEFAULT_DELAY_TIME 0
-#define DETECTION_INTERVAL 10
+#define D_DEFAULT_DELAY_TIME 0
+#define D_DETECTION_INTERVAL 10
 
-#define DRIVE_TIMER TIMER2_BASE
+//#define D_DRIVE_TIMER TIMER2_BASE
 
-#define DEBUG_PRINT 0  // 1でデバッグ出力を有効化、0で無効化
+#define D_DEBUG_PRINT 0  // 1でデバッグ出力を有効化、0で無効化
 
-#if DEBUG_PRINT
+#if D_DEBUG_PRINT
 #define DEBUG_LOG(fmt, ...) tm_printf(fmt, ##__VA_ARGS__)
 #else
 #define DEBUG_LOG(fmt, ...) ((void)0)
 #endif
 
-static UINT start_time = 0;
+UINT start_time = 0;
 
-#define LIST_MIN 4 //指示四つ文
-volatile BOOL request_sent_flag = FALSE;
-volatile UH last_request_list_count = 0;
+#define D_LIST_MINIMUM_NUMBER 4 //リストがこの数を下回ったら、リクエスト送信
 
-UINT read_timer_value() {
-    // タイマーのキャプチャタスクをトリガー
-    out_w(DRIVE_TIMER + TIMER_TASKS_CAPTURE(0), 1);
-    
-    // キャプチャされた値を読み取り
-    return (UINT)in_w(DRIVE_TIMER + TIMER_CC(0));
+BOOL d_request_sent_flag = FALSE;
+UH d_last_request_list_count = 0;
+
+// 物理タイマーのクロック(MHz単位)
+const INT d_timer_clock_mhz = 16;
+
+// ハンドラの起動周期(μs単位)、1000μs＝1ms
+const INT d_timer_cycle_micros = 1000;
+
+// 物理タイマーの上限値
+INT d_timer_limit = d_timer_cycle_micros * d_timer_clock_mhz - 1;
+
+// 経過時間をカウントする変数
+UW d_elapsed_count_s = 0;
+void d_time_measurement_handler(void *exinf)
+{
+    d_elapsed_count_s++;
 }
-void initialize_timer(){
-        // 高周波クロックを開始
-    out_w(CLOCK_TASKS_HFCLKSTART, 1);
-    while (!(in_w(CLOCK_EVENTS_HFCLKSTARTED)));
+T_DPTMR d_time_measurement_timer = {0, TA_HLNG, &d_time_measurement_handler};
 
-    // タイマーの設定
-    out_w(DRIVE_TIMER + TIMER_TASKS_STOP, 1);  // タイマーを停止
-    out_w(DRIVE_TIMER + TIMER_TASKS_CLEAR, 1);  // タイマーをクリア
-    out_w(DRIVE_TIMER + TIMER_MODE, 0);
-    // モード0: タイマーモード（イベント発生時にカウンタが増加）
-    out_w(DRIVE_TIMER + TIMER_BITMODE, 3);
-    // ビットモード3: 32ビットタイマー（0から2^32-1までカウント可能）
-    out_w(DRIVE_TIMER + TIMER_PRESCALER, 4);
-    // プリスケーラー値4: 16MHz / 2^4 = 1MHz
-    // タイマーのティック間隔は1μs（1MHz）となる
-}
-
-
-void start_timer() {
-    out_w(DRIVE_TIMER + TIMER_TASKS_CLEAR, 1); 
-    out_w(DRIVE_TIMER + TIMER_CC(0), 0xFFFFFFFF);
-    // キャプチャ/コンペア値を最大値に設定
-    // これにより、タイマーは連続的に動作し、オーバーフローしない
-
-    // タイマーを開始
-    out_w(DRIVE_TIMER + TIMER_TASKS_START, 1);
-    start_time = read_timer_value();
+void initialize_timer(UINT timer_number)
+{
+    DefinePhysicalTimerHandler(timer_number, &d_time_measurement_timer);
 }
 
+void start_timer(UINT timer_number)
+{
 
-// タイマーを停止し、経過時間（ミリ秒）を返す関数
-UINT stop_timer() {
-    UINT end_time = read_timer_value();
-    UINT elapsed;
-    if (end_time >= start_time) {
-        elapsed = end_time - start_time;
-    } else {
-        elapsed = (0xFFFFFFFF - start_time) + end_time + 1;
-    }
-    return elapsed / 1000;  // μsからmsへ変換
+    // 経過時間をリセット
+    d_elapsed_count_s = 0;
+
+    // 物理タイマーを起動
+    StartPhysicalTimer(timer_number, d_timer_limit, TA_CYC_PTMR);
+}
+
+UW stop_timer(UINT timer_number)
+{
+    StopPhysicalTimer(timer_number);
+    return d_elapsed_count_s;
 }
 
 // 交差点かどうかを判定する関数
 BOOL is_intersection() {
     return read_line_state(MAQUEEN_LINE_SENSOR_L2) || read_line_state(MAQUEEN_LINE_SENSOR_R2);
-}
-
-
-
-// 残り時間を待機する関数
-void wait_remaining_time(UINT elapsed_time) {
-    if (elapsed_time < TARGET_INTERVAL) {
-        stop_all_motor();
-        UINT remaining_time = TARGET_INTERVAL - elapsed_time;
-        DEBUG_LOG("Addtional waiting time: %d ms\n", remaining_time);
-        tk_slp_tsk(remaining_time);
-    }
 }
 
 // ライントラッキングを行う関数
@@ -108,16 +86,16 @@ void line_tracking() {
 
         // センサーの状態に応じてモーターを制御
         if (middle) {
-            control_motor(BOTH_MOTOR, MAQUEEN_MOVE_FORWARD, FORWARD_SPEED);
+            control_motor(BOTH_MOTOR, MAQUEEN_MOVE_FORWARD, D_FORWARD_SPEED);
         } else if (right) {
-            control_motor(LEFT_MOTOR, MAQUEEN_MOVE_FORWARD, FORWARD_SPEED);
-            control_motor(RIGHT_MOTOR, MAQUEEN_MOVE_FORWARD, STOP);
+            control_motor(LEFT_MOTOR, MAQUEEN_MOVE_FORWARD, D_FORWARD_SPEED);
+            control_motor(RIGHT_MOTOR, MAQUEEN_MOVE_FORWARD, D_STOP);
         } else if (left) {
-            control_motor(LEFT_MOTOR, MAQUEEN_MOVE_FORWARD, STOP);
-            control_motor(RIGHT_MOTOR, MAQUEEN_MOVE_FORWARD, FORWARD_SPEED);
+            control_motor(LEFT_MOTOR, MAQUEEN_MOVE_FORWARD, D_STOP);
+            control_motor(RIGHT_MOTOR, MAQUEEN_MOVE_FORWARD, D_FORWARD_SPEED);
         }
 
-		tk_slp_tsk(DETECTION_INTERVAL);  // ms待機
+		tk_slp_tsk(D_DETECTION_INTERVAL);  // ms待機
     }
 }
 
@@ -125,8 +103,8 @@ void line_tracking() {
 void turn_right() {
     DEBUG_LOG("Start Right Turn\n");
 
-    control_motor(LEFT_MOTOR, MAQUEEN_MOVE_FORWARD, FORWARD_SPEED);
-    control_motor(RIGHT_MOTOR, MAQUEEN_MOVE_BACKWARD, BACKWARD_SPEED);
+    control_motor(LEFT_MOTOR, MAQUEEN_MOVE_FORWARD, D_FORWARD_SPEED);
+    control_motor(RIGHT_MOTOR, MAQUEEN_MOVE_BACKWARD, D_BACKWARD_SPEED);
 
 
 
@@ -150,7 +128,7 @@ void turn_right() {
             }
         }
 
-         tk_slp_tsk(DETECTION_INTERVAL);  // ms待機
+         tk_slp_tsk(D_DETECTION_INTERVAL);  // ms待機
     }
   
 }
@@ -159,8 +137,8 @@ void turn_right() {
 void turn_left() {
     DEBUG_LOG("Start Right Turn\n");
 
-    control_motor(LEFT_MOTOR, MAQUEEN_MOVE_BACKWARD, BACKWARD_SPEED);
-    control_motor(RIGHT_MOTOR, MAQUEEN_MOVE_FORWARD, FORWARD_SPEED);
+    control_motor(LEFT_MOTOR, MAQUEEN_MOVE_BACKWARD, D_BACKWARD_SPEED);
+    control_motor(RIGHT_MOTOR, MAQUEEN_MOVE_FORWARD, D_FORWARD_SPEED);
   
     BOOL complete_firststep = FALSE;//右折開始後、L1MR1がラインを離れたらTrue
     while (TRUE) {
@@ -181,15 +159,15 @@ void turn_left() {
                 break;  // ループを抜けて回転を終了
             }
         }
-        tk_slp_tsk(DETECTION_INTERVAL);  // 10ms待機
+        tk_slp_tsk(D_DETECTION_INTERVAL);  // 10ms待機
     }
 }
 
-void follow_path(Order order) {
-    UINT duration_ms = get_order_duration(order);
-    UINT actual_duration = 0;
+void follow_path(Order order,INT timer_number) {
+    UINT duration_s = get_order_duration(order);
+    UINT actual_duration_ms = 0;
 
-    start_timer();
+    start_timer(timer_number);
 
     if(is_forward(order)){
         clear_led();
@@ -207,78 +185,75 @@ void follow_path(Order order) {
         turn_right();
     }
 
-    actual_duration = stop_timer();
+    actual_duration_ms = stop_timer(timer_number);
     
     // 指定された時間まで待機
-    if (actual_duration < duration_ms * 1000) {  // durationは秒単位
-        DEBUG_LOG("(Required Time: %d ms)\n", actual_duration);
-        DEBUG_LOG("Addtional waiting time: %d ms\n", duration_ms * 1000 - actual_duration);
+    if (actual_duration_ms< duration_s * 1000) {  // durationは秒単位
+        DEBUG_LOG("(Required Time: %d ms)\n", actual_duration_ms);
+        DEBUG_LOG("Addtional waiting time: %d ms\n", duration_s * 1000 - actual_duration_ms);
         
         stop_all_motor();
-        tk_slp_tsk(duration_ms * 1000 - actual_duration);
+        tk_slp_tsk(duration_s * 1000 - actual_duration_ms);
     }
 }
 
-
-
-void start_drive(UB timer_number) {
+void start_drive(UINT timer_number) {
     List* order_list=list_init();//経路を保存するリストの作成
 
-    initialize_timer();//タイマの初期化
+    initialize_timer(timer_number);//タイマの初期化
     maqueen_init();//maqueenの初期化
 
     UINT departure_ms=request_departure_time_ms();//待機時間受け取り
     tm_printf("Departure Time: %d\n", departure_ms);
 
-    UINT departure_second = departure_ms / 1000;
-    reserve_order(order_list,departure_second);//listをグローバル変数にするとともに、送信タスクを起動
+    UINT departure_s = departure_ms / 1000;
+    reserve_order(order_list,departure_s);//listをグローバル変数にするとともに、送信タスクを起動
     tk_slp_tsk(departure_ms);//侵入可能時間まで待機
 
 
     while(TRUE){
-        if(list_length(order_list)<LIST_MIN){
-            INT departure_delay= calculate_departure_delay(order_list);
+        if(list_length(order_list)<D_LIST_MINIMUM_NUMBER){
+            INT departure_delay_s= calculate_departure_delay_s(order_list);
             process_orders(order_list);
         }
 
         void *data=list_get(order_list,0);
         Order* order = (Order*)data;
         if(data !=NULL){
-            follow_path(*order);
+            follow_path(*order,timer_number);
         }
         list_shift(order_list);
     }
 }
 
-
-INT calculate_departure_delay(List *order_list) { //リストにある経路の所要時間を計算
-    UB delay_until_departure = 0;
+INT calculate_departure_delay_s(List *order_list) { //リストにある経路の所要時間を計算
+    UB delay_until_departure_s = 0;
     Element *pointer = order_list->head;
 
     while (pointer != order_list->tail && pointer != NULL) {
-        delay_until_departure += get_order_duration((Order*)pointer->data);
+        delay_until_departure_s += get_order_duration((Order*)pointer->data);
         pointer = pointer->next;
     }
 
-    return delay_until_departure;
+    return delay_until_departure_s;
 }
 
 void process_orders(List *order_list) { //リクエスト後反映されるまでにリクエストを行わないようにする
     UH current_list_count = list_length(order_list);
 
-    if (!request_sent_flag && current_list_count <= 4) {
+    if (!d_request_sent_flag && current_list_count <= 4) {
         // リクエストを送信
-        INT departure_delay = calculate_departure_delay(order_list);
-        reserve_order(order_list, departure_delay);
+        INT departure_delay_s = calculate_departure_delay_s(order_list);
+        reserve_order(order_list, departure_delay_s);
 
         // フラグを設定し、リスト個数を保存
-        request_sent_flag = TRUE;
-        last_request_list_count = current_list_count;
-    } else if (request_sent_flag) {
+        d_request_sent_flag = TRUE;
+        d_last_request_list_count = current_list_count;
+    } else if (d_request_sent_flag) {
         // リスト個数が増加したかチェック
-        if (current_list_count > last_request_list_count) {
+        if (current_list_count > d_last_request_list_count) {
             // リスト個数が増加したのでフラグをリセット
-            request_sent_flag = FALSE;
+            d_request_sent_flag = FALSE;
         }
     }
 }
