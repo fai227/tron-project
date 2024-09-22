2a0f9f9ea73ab2af6eb8ac37de256c51dec5e20f
umi-to <15.umeboshi.49@gmail.com>
2024-09-22 14:27:19 +0900

    ファイル整理

--- build_make/mtkernel_3/CAV/subdir.mk
+++ build_make/mtkernel_3/CAV/subdir.mk
@@ -2,14 +2,14 @@
 # micro T-Kernel 3.00.03  makefile
 ################################################################################
 
-TEMP_SRCS = $(wildcard ../app_sample/*.c)
+TEMP_SRCS = $(wildcard ../CAV/src/*.c)
 TEMP_OBJS = $(TEMP_SRCS:.c=.o)
 TEMP_DEPS = $(TEMP_SRCS:.c=.d)
 
 OBJS += $(subst ../, ./mtkernel_3/, $(TEMP_OBJS))
 C_DEPS += $(subst ../, ./mtkernel_3/, $(TEMP_DEPS))
 
-mtkernel_3/app_sample/%.o: ../app_sample/%.c
+mtkernel_3/CAV/src/%.o: ../CAV/src/%.c
 	@echo 'Building file: $<'
 	$(GCC) $(CFLAGS) -D$(TARGET) $(INCPATH) -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
 	@echo 'Finished building: $<'
