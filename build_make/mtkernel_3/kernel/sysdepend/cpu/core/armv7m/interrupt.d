d7b513430b6895b44c834deb627ebcbd0cfbceb8
umi-to <15.umeboshi.49@gmail.com>
2024-09-25 13:29:40 +0900

    ビルド

--- build_make/mtkernel_3/kernel/sysdepend/cpu/core/armv7m/interrupt.d
+++ build_make/mtkernel_3/kernel/sysdepend/cpu/core/armv7m/interrupt.d
@@ -36,7 +36,7 @@ mtkernel_3/kernel/sysdepend/cpu/core/armv7m/interrupt.o: \
  ../kernel/sysdepend/cpu/core/armv7m/../../../sysdepend.h \
  ../kernel/sysdepend/cpu/core/armv7m/../../../microbit/sysdepend.h \
  ../kernel/sysdepend/cpu/core/armv7m/../../../microbit/../cpu/nrf5/sysdepend.h \
- c:\users\admin\documents\github\tron-project\kernel\sysdepend\cpu\core\armv7m\sysdepend.h
+ c:\users\15ume\documents\github\tron-project\kernel\sysdepend\cpu\core\armv7m\sysdepend.h
 ../include/sys/machine.h:
 ../config/config.h:
 ../config/config_func.h:
@@ -89,4 +89,4 @@ mtkernel_3/kernel/sysdepend/cpu/core/armv7m/interrupt.o: \
 ../kernel/sysdepend/cpu/core/armv7m/../../../sysdepend.h:
 ../kernel/sysdepend/cpu/core/armv7m/../../../microbit/sysdepend.h:
 ../kernel/sysdepend/cpu/core/armv7m/../../../microbit/../cpu/nrf5/sysdepend.h:
-c:\users\admin\documents\github\tron-project\kernel\sysdepend\cpu\core\armv7m\sysdepend.h:
+c:\users\15ume\documents\github\tron-project\kernel\sysdepend\cpu\core\armv7m\sysdepend.h:
